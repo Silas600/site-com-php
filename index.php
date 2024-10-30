b4060f5451e4f4a0a7e2eaa352c35feff2363d54
Silas600 <silasrochsmedeiros.senac@gmail.com>
2024-10-30 14:16:59 -0300

    Site em php

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site</title>
+</head>
+<body>
+    <header>
+        <h1>Site exemplo</h1>
+        <nav>
+            <a href=""></a>
+            <a href=""></a>
+            <a href=""></a>
+        </nav>
+    </header>
+
+    <main> 
+        <article>
+            <h2>Bem-vindo(a)!</h2>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nam, assumenda!</p>
+        </article>
+    </main>
+
+    <footer>
+        <h2>Site criado como exemplo</h2>
+    </footer>
+
+</body>
+</html>
